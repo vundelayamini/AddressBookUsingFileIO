b9d477e3c99593688185118e307d9049c2b2714e
vundelayamini <vundelayamini@gmail.com>
2021-05-11 16:09:46 +0530

    [yamini]Add: Welcome message to addressbook using fileio

---
+++ AddressBookFileIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookFileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Addressbook using FileIo");
+        }
+    }
+}
