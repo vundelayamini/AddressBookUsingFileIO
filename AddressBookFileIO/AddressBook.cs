7a89a5d0215e2122a3fca58234ae1fa751031c15
vundelayamini <vundelayamini@gmail.com>
2021-05-11 17:16:23 +0530

    [yamini]Add: Sorting the name,city and zip

--- AddressBookFileIO/AddressBook.cs
+++ AddressBookFileIO/AddressBook.cs
@@ -290,11 +290,73 @@ namespace AddressBookFileIO
                     }
                 }
             }
+        /// <summary>
+        /// UC12-Ability to sort the entries in the address book by City, State, or Zip
+       /// </summary>
+        public void SortByCity()
+        {
+            CreateCityDictionary();
+            Dictionary<string, Contact> inverseCityDictionary = new Dictionary<string, Contact>();
+            foreach (AddressBook obj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in obj.cityDictionary.Keys)
+                {
+                    inverseCityDictionary.TryAdd(contact.City, contact);
+                }
+            }
+            List<string> list = inverseCityDictionary.Keys.ToList();
+            list.Sort();
+            foreach (string city in list)
+            {
+                Console.WriteLine(inverseCityDictionary[city].ToString());
+            }
+        }
+        /// <summary>
+        /// Ability to sort the entries in the address book by state
+        /// </summary>
+        public void SortByState()
+        {
+            CreateStateDictionary();
+            Dictionary<string, Contact> inverseStateDictionary = new Dictionary<string, Contact>();
+            foreach (AddressBook obj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in obj.stateDictionary.Keys)
+                {
+                    inverseStateDictionary.TryAdd(contact.State, contact);
+                }
+            }
+            List<string> list = inverseStateDictionary.Keys.ToList();
+            list.Sort();
+            foreach (string state in list)
+            {
+                Console.WriteLine(inverseStateDictionary[state].ToString());
+            }
+        }
+        /// <summary>
+        /// Ability to sort the entries in the address book by  Zip
+        /// </summary>
+        public void SortByZip()
+        {
+            SortedList<int, Contact> sortedbyCity = new SortedList<int, Contact>();
+            foreach (AddressBook addressBookobj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in addressBookobj.addressBook.Values)
+                {
+                    sortedbyCity.TryAdd(contact.Zip, contact);
+                }
+            }
+            foreach (var item in sortedbyCity)
+            {
+                Console.WriteLine(item.Value.ToString());
+            }
+        }
 
-        
     }
 
+
 }
+
+
 
 
 
