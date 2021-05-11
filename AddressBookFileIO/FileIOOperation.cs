5e9da4ea2848d7b48ffa60190e2fda034734b66f
vundelayamini <vundelayamini@gmail.com>
2021-05-11 17:34:52 +0530

    [yamini]Add: Ability to Read or Write

---
+++ AddressBookFileIO/FileIOOperation.cs
@@ -0,0 +1,34 @@
+﻿using Cake.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBookFileIO
+{
+    class FileIOOperation
+    {
+        public void WriteToFile(Dictionary<string, AddressBook> addressBookDictionary)
+        {
+             string FilePath = @"C:\Users\mahesh\source\repos\AddressBookFileIO\AddressBookFileIO\Sample.txt";
+             StreamWriter writer = new StreamWriter(FilePath, true);
+            foreach (AddressBook addressBookobj in addressBookDictionary.Values)
+            {
+                foreach (Contact contact in addressBookobj.addressBook.Values)
+                {
+                    writer.WriteLine(contact.ToString());
+                }
+            }
+            Console.WriteLine("\nSuccessfully added to Text file.");
+            writer.Close();
+        }
+        public void ReadFromFile()
+        {
+            Console.WriteLine("Below are Contents of Text File");
+            string lines = File.ReadAllText(FilePath);
+            Console.WriteLine(lines);
+        }
+    }
+}
+
+
